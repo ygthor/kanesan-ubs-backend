7190cf7d37bc1daf9c18a898f4b5d791a68bf7c5
Thor Yu Guan <luckboy5566@gmail.com>
2025-08-22 00:30:23 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\SyncController;
 // Correctly namespacing the CustomerController we've been working on
 use App\Http\Controllers\Api\CustomerController; // <<< Ensure this is the controller from laravel_customer_controller_v2
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\GlStatementController;
+
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ReceiptController;
 use App\Http\Controllers\Api\UserController;
