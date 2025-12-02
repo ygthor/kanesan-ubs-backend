bf1e6541abcb26556e5a2a9a9f0823657bcb93f8
Thor Yu Guan <luckboy5566@gmail.com>
2025-12-03 01:12:05 +0800

    s

--- app/Http/Controllers/Api/InvoiceItemController.php
+++ app/Http/Controllers/Api/InvoiceItemController.php
@@ -102,8 +102,12 @@ class InvoiceItemController extends Controller
                     
                     if ($linkedInvoice) {
                         // Check if the product code exists in the linked invoice items
+                        // Check both ITEMNO and TRANCODE as they should be the same
                         $linkedInvoiceItem = ArTransItem::where('artrans_id', $linkedInvoice->artrans_id)
-                            ->where('ITEMNO', $request->product_code)
+                            ->where(function($query) use ($request) {
+                                $query->where('ITEMNO', $request->product_code)
+                                      ->orWhere('TRANCODE', $request->product_code);
+                            })
                             ->first();
                         
                         if (!$linkedInvoiceItem) {
